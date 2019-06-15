a73b0099af79817dd0edd9a7966b90d81c48cea3
Piotr <latidude99@gmail.com>
2019-06-15 16:51:42 +0100

    Add error page
    Add general error landing page and a custom exception hanlder
    redirecting all Exceptions to it
    (apart from multipart file upload size exception)

--- src/main/java/com/latidude99/web/controller/CustomExceptionHandler.java
+++ src/main/java/com/latidude99/web/controller/CustomExceptionHandler.java
@@ -24,17 +24,30 @@ package com.latidude99.web.controller;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.multipart.MaxUploadSizeExceededException;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
+@EnableWebMvc
 @ControllerAdvice
-public class FileUploadExceptionAdvice {
+public class CustomExceptionHandler {
 
     @ExceptionHandler(MaxUploadSizeExceededException.class)
     public String handleMaxSizeException(MaxUploadSizeExceededException exc,
                                          HttpServletRequest request, HttpServletResponse response) {
         return "redirect:/enquiry/form/uploadfail";
+    }
+
+    @ExceptionHandler({ Exception.class })
+    public String handleAll(Exception ex, Model model) {
+        return "enquiryError";
     }
 
 }
